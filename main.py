3913825b05b65952baf8b0e54697a8cda2620fbd
asomakia <asomakia@student.bridgew.edu>
2022-02-02 15:56:17 -0500

    added sample project

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ def main():
     print("just doing other work here")
     user_response = int(input("what is your age?"))
     half_age = user_response/2
-    #this a change sw
+    #this a change
+    # this is also a change
+
     print(f"{half_age} is half your age")
 
 main()
