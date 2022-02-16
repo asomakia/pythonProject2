7ab5cca234145ad5b5ccfc7710694a4d0faedcf4
asomakia <asomakia@student.bridgew.edu>
2022-02-16 16:31:19 -0500

    added sample project

---
+++ filedrawdemo.py
@@ -0,0 +1,31 @@
+import random
+
+import arcade
+
+def main():
+    our_colors = [arcade.color.LAVENDER,arcade.color.BLUE,arcade.color.GREEN,
+                  arcade.color.BOSTON_UNIVERSITY_RED,arcade.color.ORANGE,
+                  arcade.color.ALIZARIN_CRIMSON,arcade.color.ARSENIC,
+                  arcade.color.ATOMIC_TANGERINE,arcade.color.YELLOW]
+    arcade.open_window(800,800,"bringing together the last few weeks")
+    rectangleFile = open("recfile.txt","r")
+
+    all_lines = rectangleFile.readlines()
+
+    arcade.start_render()
+    for rectangle in all_lines:
+        rec_data = rectangle.split(",")
+        center_x = int(rec_data[0])
+        center_y = int(rec_data[1])
+        width = int(rec_data[2])
+        height = int(rec_data[3])
+        color = random.choice(our_colors)
+        drawing = arcade.create_rectangle(center_x,center_y,width,height,color)
+        drawing.draw()
+
+
+
+
+    arcade.finish_render()
+    arcade.run()
+main()
